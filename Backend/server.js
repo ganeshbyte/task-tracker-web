54ce0329b5b1598e10727aae79c1b9619a391e71
ganeshbyte <gc930954@gmail.com>
2024-01-26 12:07:53 +0530

    added server logic

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,83 @@
 const express = require("express");
-
+const { createTodoValidator, updateTodoValidator } = require("./middleware/TodoValidator");
+const connectToDb = require("./db/connect");
+const Todo = require("./model/todo");
+const winston = require("winston");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const port = process.env.PORT || 3000;
+
+//set up logger
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'audit.log' })
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple(),
+
+    )
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
-app.listen(3000, () => {
-    console.log(`Server is started at port 3000`);
+app.get("/todos", async (req, res) => {
+    try {
+        const todos = await Todo.find({});
+        logger.info(`Todo get`);
+        res.status(200).json(todos);
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+app.post("/todo", createTodoValidator, async (req, res) => {
+    try {
+        const { title, description } = req.body;
+
+        const todo = new Todo({
+            title,
+            description,
+            completed: false
+        })
+        await todo.save();
+        logger.info(`Todo saved`);
+        res.status(200).json(todo);
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+app.put("/completed", updateTodoValidator, async (req, res) => {
+    try {
+        const { id } = req.body;
+        const todo = await Todo.findByIdAndUpdate({ _id: id }, { completed: true }, { new: true });
+        logger.info(`Todo updated`);
+        res.status(200).json(todo);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
+
+const start = async () => {
+    try {
+        await connectToDb("mongodb://127.0.0.1:27017/todo");
+        console.log("Connected to db ...");
+        app.listen(port, () => {
+            console.log(`Server is started at port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
